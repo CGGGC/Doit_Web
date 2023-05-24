bf6aa9f96b1a74a705f197833c02c5de804e0441
CGGGC <122100058+CGGGC@users.noreply.github.com>
2023-05-24 23:02:47 +0900

    [Do it!] js

---
+++ js/15_06_01_이벤트처리기.js
@@ -0,0 +1,19 @@
+// 클릭시 배경이 바뀌도록 적용
+function background_green(color) {
+    let result = document.querySelector('#result');
+    result.style.backgroundColor = color;
+    console.log(result);
+}
+
+function background_orange(color) {
+    let result = document.querySelector('#result');
+    result.style.backgroundColor = color;
+    console.log(result);
+}
+
+function background_purple(color) {
+    let result = document.querySelector('#result');
+    result.style.backgroundColor = color;
+    console.log(result);
+    console.log("hi");
+}
