329f19109eb91e26dff00e79181bb69ce5fa3789
CGGGC <122100058+CGGGC@users.noreply.github.com>
2023-06-26 23:14:23 +0900

    [Do it!] js

---
+++ html/17_03_02_라이트박스 효과 만들기.js
@@ -0,0 +1,8 @@
+function click_img() {
+    // 이미지의 요소를 모두 가져옴
+    let img = document.getElementsByClassName("get_img");
+    // 라이트박스 영역의 요소를 가져옴
+    let lightbox_img = document.getElementById("box_02");
+    // 라이트박스 이미지 요소를 가져옴
+    let lightbox = document.getElement
+}
